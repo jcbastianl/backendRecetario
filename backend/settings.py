da61907d481d34221a7690900f7a04e27436e56a
jcbastianl <joseph.balcazar@unl.edu.ec>
2025-08-15 17:50:32 -0500

    Movida en middlewares

--- backend/settings.py
+++ backend/settings.py
@@ -50,17 +50,18 @@ INSTALLED_APPS = [
     'seguridad'
 ]
 
+# backend/settings.py
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # <-- MOVIDO HACIA ARRIBA
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
-
 
 
 
